2a9700d0524d94f0f8cca1e101ce5e9c981d14a3
R-salton <ringusa22@gmail.com>
2023-08-16 21:25:15 +0200

    Added Profile page

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group, User
+from .models import Profile
 
-# Register your models here.
+# Unregister user
+
+admin.site.unregister(Group)
+
+class ProfileInline(admin.StackedInline):
+    model = Profile
+# extends USer MOdel
+
+# Combine User and Profile
+class UserAdmin(admin.ModelAdmin):
+    model = User
+    # Display USer Fields
+    fields = ["username"]
+    inlines = [ProfileInline]
+
+# unregister user Initial User
+admin.site.unregister(User)
+# Register user and pr eofile
+
+admin.site.register(User, UserAdmin)
+# admin.site.register(Profile)
--- core/views.py
+++ core/views.py
@@ -1,6 +1,30 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.shortcuts import render, redirect
+from .models import Profile
+from django.urls import reverse
+from django.http import HttpResponseRedirect
+
 
 # Create your views here.
 
 def home(request):
-    return render(request,'home.html')
+    return render(request, 'home.html')
+
+
+def profile_list(request, massege=None):
+    if request.user.is_authenticated:
+        profiles = Profile.objects.exclude(user=request.user)
+        return render(request, 'profile_list.html', {'profiles': profiles})
+    else:
+        messages.success(request, 'You Must First Log in to continue..!')
+
+        return HttpResponseRedirect('/')
+
+def profile(request, pk):
+    if request.user.is_authenticated:
+        profile = Profile.objects.get(user_id=pk)
+        return  render(request, 'profile.html', {"profile": profile})
+    else:
+        messages.success(request, 'You Must First Log in to continue..!')
+
+        return HttpResponseRedirect('/')
